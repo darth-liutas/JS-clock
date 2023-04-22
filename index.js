ddc88a669ba12aa1c4024dc1e77050465d276e71
angrywolfjr <54941790+darth-liutas@users.noreply.github.com>
2023-04-22 14:17:09 +0100

    i forgor to add the whole PM AM thing

--- index.js
+++ index.js
@@ -7,11 +7,18 @@ minutes=document.getElementById("minutes");
 seconds=document.getElementById("seconds");
 function test(){
 d = new Date(); // for now
-hour=d.getHours(); // => 9
-minute=d.getMinutes(); // =>  30
-second=d.getSeconds(); // => 51
-hours.innerHTML=hour;
-minutes.innerHTML=minute;
+hour=d.getHours(); 
+if(hour>=12){hour-=12;
+format.innerHTML="PM";
+}
+else{format.innerHTML="AM";}
+minute=d.getMinutes(); 
+second=d.getSeconds(); 
+if(hour<10){hours.innerHTML="0"+hour;}
+else {hours.innerHTML=hour;}
+if(minute<10){minutes.innerHTML="0"+minute;}
+else {minutes.innerHTML=minute;}
+
 seconds.innerText=second;
 setTimeout(test, 1000);
 }
